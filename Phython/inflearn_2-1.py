7e4f52906bf4530a49fc577b935a7dad620c43c8
gwbaik9717@icloud.com <gwbaik9717@icloud.com>
2023-10-29 11:59:27 +0900

    파이썬 연습 인프런 2-1 K번째 수

---
+++ Phython/inflearn_2-1.py
@@ -0,0 +1,13 @@
+sample = "5 3"
+
+n, k = map(int, sample.split(' '))
+cnt = 0
+
+for i in range(1, n+1):
+    if(n % i == 0):
+        cnt += 1
+        if(cnt == k):
+            print(i)
+            break
+else:
+    print(-1)
