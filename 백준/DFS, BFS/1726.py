a8684bd6f9155db34ea71f989dfe2418844cb136
Paik <gwbaik9717@icloud.com>
2024-03-31 10:50:21 +0900

    BOJ_1726 로봇 문제풀이

--- 백준/DFS, BFS/1726.py
+++ 백준/DFS, BFS/1726.py
@@ -10,6 +10,7 @@ visited = [[[0] * 4 for _ in range(m)] for _ in range(n)]
 dy = [0, 0, 1, -1]
 dx = [1, -1, 0, 0]
 dm = [1, 2, 3]
+new_dir = [((3,1), (2,1), (1,2)), ((3, 1), (2, 1), (0, 2)), ((0, 1), (1, 1), (3, 2)), ((0, 1), (1, 1),(2, 2))]
 
 visited[sy][sx][sd] = 1
 q = deque()
@@ -17,42 +18,27 @@ q.append([sy, sx, sd, 0])
 
 while q:
     [cy, cx, cd, cc] = q.popleft()
+    
+    if (cy, cx, cd) == (ey, ex, ed):
+        print(cc)
+        break
+    
+    # 움직일때
+    for i in range(3):
+        nx = cx + dx[cd] * dm[i]
+        ny = cy + dy[cd] * dm[i]
 
-    if (cy, cx) == (ey, ex):
-        if cd == ed:
-            print(cc)
-            break
-        else:
-            # 이렇게 하면 최소 거리를 보장하지 못한다.
-            # 따라서 이렇게 풀면 안된다.
-            if cd // 2 == ed // 2:
-                print(cc + 2)
+        if nx >= 0 and nx < m and ny >= 0 and ny < n:
+            if graph[ny][nx] == 1:
                 break
+            elif visited[ny][nx][cd] == 1:
+                continue
             else:
-                print(cc + 1)
-                break
+                visited[ny][nx][cd] = 1
+                q.append([ny, nx, cd, cc + 1])
 
-    for i in range(4):
-        for j in range(3):
-            ny = cy + dy[i] * dm[j]
-            nx = cx + dx[i] * dm[j]
-
-            if nx >= 0 and nx < m and ny >= 0 and ny < n and graph[ny][nx] == 0:
-                # 같은 방향을 바라보고 있을때
-                if i == cd:
-                     if visited[ny][nx][cd] == 0:
-                       visited[ny][nx][cd] = 1
-                       q.append([ny, nx, cd, cc + 1])
-                # 다른 방향을 바라보고 있을때
-                else:
-                    nd = i
-                    if visited[ny][nx][nd] == 0:
-                        visited[ny][nx][nd] = 1
-
-                        # 2 만큼 회전
-                        if i // 2 == cd // 2:
-                            q.append([ny, nx, nd, cc + 3])
-                        else:
-                            q.append([ny, nx, nd, cc + 2])
-            else:
-                break
+    # 회전할때
+    for nd, dc in new_dir[cd]:
+        if visited[cy][cx][nd] == 0:
+            visited[cy][cx][nd] = 1
+            q.append([cy, cx, nd, cc + dc])
