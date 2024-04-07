d0424161bf33c888efbf18d4933b4bea4fb69139
Paik <gwbaik9717@icloud.com>
2024-04-07 17:21:59 +0900

    BOJ_2293 동전1 문제풀이

--- 백준/DP/2293.py
+++ 백준/DP/2293.py
@@ -1,22 +1,15 @@
 import sys
-
 n, k = map(int, sys.stdin.readline().split())
 
 coins = [int(sys.stdin.readline()) for _ in range(n)]
 coins.sort()
 
-table = [0] * ((k + 1) * (n + 1))
+# 메모리 제한으로 1차원 배열로 변경
+dp = [0] * (k + 1)
+dp[0] = 1
 
-for i in range(n + 1):
-    table[i * (k + 1)] = 1
-    
-for i in range(1, n+1):
-    currentcoin = coins[i-1]
+for c in coins:
+    for i in range(c, k + 1):
+        dp[i] += dp[i - c]
 
-    for j in range(0, k+1):
-        if j - currentcoin < 0:
-            table[i * (k + 1) + j] = table[(i - 1) * (k + 1) + j]
-        else:
-            table[i * (k + 1) + j] = table[(i - 1) * (k + 1) + j] + table[i * (k + 1) + j - currentcoin]
-
-print(table[-1])
+print(dp[k])
