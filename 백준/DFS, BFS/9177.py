ec4045db7d2f1ef972e10ef97cc27684fb4b7680
Paik <gwbaik9717@icloud.com>
2024-04-12 08:24:32 +0900

    BOJ_9177 단어 섞기 BFS 문제풀이 - visited 배열 적용 전

--- 백준/DFS, BFS/9177.py
+++ 백준/DFS, BFS/9177.py
@@ -1,30 +1,35 @@
 import sys
+from collections import deque
 
 n = int(sys.stdin.readline().strip())
 tcs = [sys.stdin.readline().strip().split() for _ in range(n)]
 
-def dfs(cur, cci, tc):
+
+def bfs(tc):
     global ans
+  
+    q = deque()
+    q.append(["", [0, 0]])
 
-    [a, b, target] = tc
-    n = len(cur)
+    while q:
+        [cw, cis] = q.popleft()
+        
+        if cw == tc[-1]:
+            ans = "yes"
+            break
 
-    if n > 0 and target[:n] != cur:
-        return 
-    if target == cur:
-        ans = "yes"
-        return 
-    
-    for i, ci in enumerate(cci):
-        if ci < len(tc[i]):
-            new_cci = cci[:]
-            new_cci[i] += 1
-            
-            dfs(cur + tc[i][ci], new_cci, tc)
+        for i, ci in enumerate(cis):
+        
+          if ci < len(tc[i]):
+            ni = ci + 1
+            ncis = cis[:]
+            ncis[i] = ni
+            nw = cw + tc[i][ci]
 
-
+            if nw == tc[-1][:len(nw)]:
+                q.append([nw, ncis])
 
 for i, tc in enumerate(tcs):
     ans = "no"
-    dfs("", [0, 0], tc)
+    bfs(tc)
     print(f'Data set {i + 1}: {ans}')
