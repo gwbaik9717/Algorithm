39d697ec8c3405ab2f328233f3a6a690e666eac3
Paik <gwbaik9717@icloud.com>
2024-09-16 14:07:59 +0900

    토스트 계란틀 (bfs)

--- Codetree/토스트 계란틀.py
+++ Codetree/토스트 계란틀.py
@@ -1,5 +1,5 @@
 import sys
-from copy import deepcopy
+from collections import deque
 
 n, L, R = map(int, sys.stdin.readline().strip().split())
 graph = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(n)]
@@ -17,42 +17,55 @@ def start_round():
 
     to_change = []
 
-    def dfs(position):
+    def bfs(position):
         nonlocal total, count
-
         count += 1
-        
+        q = deque()
+        q.append(position)
+
         cy, cx = position
         to_change.append((cy, cx))
         checked[cy][cx] = 1
         total += graph[cy][cx]
-        
-        for zipped in zip(dy, dx):
-            ny, nx = cy + zipped[0], cx + zipped[1]
 
-            if 0 <= ny < n and 0 <= nx < n and checked[ny][nx] == 0 and L <= abs(graph[ny][nx] - graph[cy][cx]) <= R:
-                dfs((ny, nx))
+        while q:
+            cy, cx = q.popleft()
 
-    
+            for zipped in zip(dy, dx):
+                ny, nx = cy + zipped[0], cx + zipped[1]
+
+                if 0 <= ny < n and 0 <= nx < n and checked[ny][nx] == 0 and L <= abs(graph[ny][nx] - graph[cy][cx]) <= R:
+                    checked[ny][nx] = 1
+                    count += 1
+                    q.append((ny, nx))
+                    to_change.append((ny, nx))
+                    total += graph[ny][nx]
+
+    found = False
+                    
     for i in range(n):
         for j in range(n):
             if checked[i][j] == 0:
-                dfs((i, j))
+                bfs((i, j))
+
+                if len(to_change) > 1:
+                    found = True
+
                 for change in to_change:
                     cy, cx = change
                     graph[cy][cx] = total // count
 
                 total, count = 0, 0
                 to_change = []
+    
+    return found
 
 while True:
-    prev = deepcopy(graph)
-    start_round()
-    
+    restart = start_round()
 
-    if prev == graph:
-        break
-    
-    ans += 1
+    if restart:
+        ans += 1
+    else:
+        break 
 
 print(ans)
