44ee43ea3eb192929de4e8ae525e50314092efe8
Paik <gwbaik9717@icloud.com>
2024-11-15 09:07:40 +0900

    BOJ_2293 동전1 2회차

---
+++ 백준/DP/2293-2.py
@@ -0,0 +1,14 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().strip().split()) 
+coins = [int(sys.stdin.readline().strip()) for _ in range(n)]
+
+dp = [0] * (k+1)
+
+dp[0] = 1
+
+for coin in coins:
+    for i in range(coin, k+1):
+        dp[i] += dp[i-coin]
+
+print(dp[-1])
