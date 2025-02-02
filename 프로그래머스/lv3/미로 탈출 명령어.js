b07a51ba7b91dae319a6e0a98d1ea7a2a0ca2da7
ganu <gwbaik9717@icloud.com>
2025-02-02 10:54:01 +0900

    Programmers 미로 탈출 명령어 greedy로 재풀이

--- 프로그래머스/lv3/미로 탈출 명령어.js
+++ 프로그래머스/lv3/미로 탈출 명령어.js
@@ -1,65 +1,90 @@
-class Queue {
-  constructor() {
-    this.q = [];
-    this.head = 0;
-    this.tail = 0;
-  }
-
-  push(value) {
-    this.q.push(value);
-    this.tail++;
-  }
-
-  pop() {
-    const rv = this.q[this.head];
-    delete this.q[this.head++];
-    return rv;
-  }
-
-  isEmpty() {
-    return this.head === this.tail;
-  }
-}
-
 function solution(n, m, x, y, r, c, k) {
-  const dir = ["d", "l", "r", "u"];
-  const dx = [1, 0, 0, -1];
-  const dy = [0, -1, 1, 0];
+  const manhattan = Math.abs(r - x) + Math.abs(c - y);
 
-  // Early return if impossible
-  const distance = Math.abs(r - x) + Math.abs(c - y);
-  if (distance > k || (k - distance) % 2 !== 0) return "impossible";
+  if (k < manhattan) {
+    return "impossible";
+  }
 
-  const queue = new Queue();
-  queue.push([x, y, ""]);
-  const visited = new Set();
-  visited.add(`${x},${y},0`);
+  if ((k - manhattan) % 2 !== 0) {
+    return "impossible";
+  }
 
-  while (!queue.isEmpty()) {
-    const [cx, cy, path] = queue.pop();
+  let down = Math.max(r - x, 0);
+  let left = Math.max(y - c, 0);
+  let right = Math.max(c - y, 0);
+  let up = Math.max(x - r, 0);
 
-    if (path.length === k) {
-      if (cx === r && cy === c) return path;
-      continue;
+  let pair = Math.floor((k - manhattan) / 2);
+  let answer = "";
+
+  let cx = x;
+  let cy = y;
+
+  for (let i = 0; i < k; i++) {
+    // Down
+    if (down || pair) {
+      if (cx < n) {
+        if (down) {
+          down -= 1;
+        } else if (pair) {
+          up += 1;
+          pair -= 1;
+        }
+
+        answer += "d";
+        cx += 1;
+        continue;
+      }
     }
 
-    for (let i = 0; i < 4; i++) {
-      const nx = cx + dx[i];
-      const ny = cy + dy[i];
-      const newPath = path + dir[i];
+    // Left
+    if (left || pair) {
+      if (cy > 1) {
+        if (left) {
+          left -= 1;
+        } else if (pair) {
+          right += 1;
+          pair -= 1;
+        }
 
-      if (
-        nx >= 1 &&
-        nx <= n &&
-        ny >= 1 &&
-        ny <= m &&
-        !visited.has(`${nx},${ny},${newPath.length}`)
-      ) {
-        queue.push([nx, ny, newPath]);
-        visited.add(`${nx},${ny},${newPath.length}`);
+        answer += "l";
+        cy -= 1;
+        continue;
+      }
+    }
+
+    // Right
+    if (right || pair) {
+      if (cy < m) {
+        if (right) {
+          right -= 1;
+        } else if (pair) {
+          left += 1;
+          pair -= 1;
+        }
+
+        answer += "r";
+        cy += 1;
+        continue;
+      }
+    }
+
+    // Up
+    if (up || pair) {
+      if (cx > 1) {
+        if (up) {
+          up -= 1;
+        } else if (pair) {
+          down += 1;
+          pair -= 1;
+        }
+
+        answer += "u";
+        cx -= 1;
+        continue;
       }
     }
   }
 
-  return "impossible";
+  return answer;
 }
