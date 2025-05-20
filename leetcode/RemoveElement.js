e4a43e8ec0a7e403b3fceb9b9a82903da4b13f42
Gunwoo Baik <gwbaik9717@icloud.com>
2025-05-20 00:37:17 +0000

    leetcode 27. Remove Element

---
+++ leetcode/RemoveElement.js
@@ -0,0 +1,20 @@
+// TC: O(n)
+// SC: O(1)
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let k = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+};
